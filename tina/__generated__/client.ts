96b6680c774a74e93299af6062e750515b40b2a5
Stefan Musch <stefan@gradientmetrics.com>
2026-03-15 10:25:51 +0100

    client.ts

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ cacheDir: 'C:/Users/Stefa/Documents/Musch Analytics B.V/Avond4daagse/avond4daagse/tina/__generated__/.cache/1773566506243', url: 'https://content.tinajs.io/2.2/content/c09d5a57-e0a3-4c20-a525-74daa3144e73/github/main', token: '843286c150a6817593ff3cc2b766f320f7448adb', queries,  });
+export const client = createClient({ cacheDir: 'C:/Users/Stefa/Documents/Musch Analytics B.V/Avond4daagse/avond4daagse/tina/__generated__/.cache/1773566725543', url: 'https://content.tinajs.io/2.2/content/c09d5a57-e0a3-4c20-a525-74daa3144e73/github/main', token: '843286c150a6817593ff3cc2b766f320f7448adb', queries,  });
 export default client;
   
